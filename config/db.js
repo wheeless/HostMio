7133eb2d566cb762fc88a456543d23408e162c7b
Kyle Wheeless <kyle@wheeless.dev>
2022-01-28 11:09:26 -0800

    mongo dev server string fix

--- config/db.js
+++ config/db.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'localhost:27017/test', {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/test',
+      {
+        useNewUrlParser: true,
+      }
+    );
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);

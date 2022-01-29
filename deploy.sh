a57d40ff7d1211b0721dd12246d1008a41d4edca
Kyle Wheeless <kyle@wheeless.dev>
2022-01-29 01:36:28 -0800

    test deploy 16

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@ echo "Installing 'node-cmd'"
 npm install node-cmd
 echo "Installing 'node-ssh'"
 npm install node-ssh
-rm -rf package-lock.json
-git reset -- hard
-git pull
 echo "Running 'deploy.js'"
 node deploy.js

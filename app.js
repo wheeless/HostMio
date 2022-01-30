dc4d263e7f3d1c22ef7c95e8362f80cd5f08a0e5
Kyle Wheeless <kyle@wheeless.dev>
2022-01-30 00:45:47 -0800

    remove v2 reference from main branch

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const env = process.env.NODE_ENV || 'development';
  * Controllers (route handlers).
  */
 const linksController = require('./controllers/links_v1');
-const linksV2Controller = require('./controllers/links_v2');
 
 app.use(express.json());
 
@@ -74,11 +73,6 @@ app.get('/api/v1/links', linksController.getLinks);
 app.post('/api/v1/links', linksController.createLink);
 app.delete('/api/v1/links/:id', linksController.deleteLink);
 app.put('/api/v1/links/:id', linksController.updateLink);
-// Controller v2 Routes
-app.get('/api/v2/links', linksV2Controller.getLinks);
-app.post('/api/v2/links', linksV2Controller.createLink);
-app.delete('/api/v2/links/:id', linksV2Controller.deleteLink);
-app.put('/api/v2/links/:id', linksV2Controller.updateLink);
 
 console.log('Launch Successful');
 module.exports = app;
--- models/UrlV2.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const urlV2Schema = new mongoose.Schema({
-  longUrl: String,
-  shortUrl: String,
-  date: { type: String, default: Date.now },
-});
-
-module.exports = mongoose.model('UrlV2', urlV2Schema);

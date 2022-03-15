aba816af9bb1edb2460d5843359ad390a310deb9
Kyle Wheeless <kyle@wheeless.dev>
2022-03-15 03:09:57 -0700

    update routes

--- app.js
+++ app.js
@@ -35,6 +35,55 @@ if (process.env.TREBLLE_APIKEY && process.env.TREBLLE_PROJECTID) {
 app.get('/api/v1/links/:shortUrl', cors(), linksController.getLink);
 app.get('/api/v2/links/:shortUrl', cors(), linksV2Controller.getLink);
 
+// if (env === 'development') {
+//   app.use(cors());
+// } else if (env === 'production') {
+//   // Customize your cors options here
+//   // Edit your ALLOWED_ORIGINS in the .env file
+//   const allowedOrigins = process.env.ALLOWED_ORIGINS;
+//   const allowedOriginsArray = allowedOrigins
+//     .split(',')
+//     .map((item) => item.trim());
+//   console.log(allowedOriginsArray);
+//   var corsOptions = {
+//     origin: function (origin, callback) {
+//       if (allowedOriginsArray.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Where TF is your access token?'));
+//       }
+//     },
+//   };
+//   app.use(cors(corsOptions));
+// } else {
+//   console.log('Error: NODE_ENV not set to development or production');
+//   process.exit(1);
+// }
+// // End of cors customization section
+
+// Connect to database
+connectDB();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Auth Controller (route handlers).
+app.post('/api/auth/signup', authController.signup);
+
+// Controller v1 Routes
+app.get('/api/v1/links', cors(), linksController.getLinks);
+app.post('/api/v1/links', cors(), linksController.createLink);
+app.delete('/api/v1/links/:id', linksController.deleteLink);
+app.put('/api/v1/links/:id', linksController.updateLink);
+// Controller v2 Routes
+app.get('/api/v2/links', linksV2Controller.getLinks);
+app.post('/api/v2/links', linksV2Controller.createLink);
+app.delete('/api/v2/links/:id', linksV2Controller.deleteLink);
+app.put('/api/v2/links/:id', linksV2Controller.updateLink);
+
 if (env === 'development') {
   app.use(cors());
 } else if (env === 'production') {
@@ -60,29 +109,6 @@ if (env === 'development') {
   process.exit(1);
 }
 // End of cors customization section
-
-// Connect to database
-connectDB();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Auth Controller (route handlers).
-app.post('/api/auth/signup', authController.signup);
-
-// Controller v1 Routes
-app.get('/api/v1/links', linksController.getLinks);
-app.post('/api/v1/links', linksController.createLink);
-app.delete('/api/v1/links/:id', linksController.deleteLink);
-app.put('/api/v1/links/:id', linksController.updateLink);
-// Controller v2 Routes
-app.get('/api/v2/links', linksV2Controller.getLinks);
-app.post('/api/v2/links', linksV2Controller.createLink);
-app.delete('/api/v2/links/:id', linksV2Controller.deleteLink);
-app.put('/api/v2/links/:id', linksV2Controller.updateLink);
 
 console.log('Launch Successful');
 

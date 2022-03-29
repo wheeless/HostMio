5e026a9857c6acae99e12ac069d0c97b1eb88cf9
Kyle Wheeless <kyle@wheeless.dev>
2022-03-29 16:54:37 -0700

    fixed 404 logic for new json handling

--- controllers/links_v1.js
+++ controllers/links_v1.js
@@ -41,7 +41,7 @@ exports.getLink = async (req, res) => {
     } else {
       return res
         .status(404)
-        .redirect(process.env.CLIENT_URL + process.env.NOT_FOUND_PATH);
+        .json({ longUrl: process.env.CLIENT_URL + process.env.NOT_FOUND_PATH });
     }
   } catch (err) {
     console.error(err);

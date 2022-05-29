d55cb502458071340ce7908d48042e41b9991403
Kyle Wheeless <kyle@wheeless.dev>
2022-05-29 15:16:19 -0700

    quick fix

--- controllers/links_v1.js
+++ controllers/links_v1.js
@@ -330,14 +330,14 @@ exports.spendPoints = async (req, res) => {
     .then((url) => {
       if (url) {
         if (99 > url.points) {
-          res
-            .status(400)
-            .json({
-              message:
-                'Not enough points, this link does not have ' +
-                url.points +
-                ' points',
-            });
+          res.status(400).json({
+            message:
+              'Not enough points, this link does not have ' +
+              req.param.points +
+              ' points. Only ' +
+              url.points +
+              ' points available.',
+          });
         } else {
           url.points = url.points - req.params.points;
           url.expireAt =

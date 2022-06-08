1a2b4330e1aaeb28a6a61cda048f9e4502826fce
Kyle Wheeless <kyle@wheeless.dev>
2022-06-08 12:30:02 -0700

    removed dead route from file router

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -4,12 +4,8 @@ const fileHandler = require('../controllers/fileHandler');
 var cors = require('cors');
 
 // File Controller (route handlers).
-router.post('/api/slack/massMail', fileHandler.slackWebhook);
-router.get(
-  '/api/files/downloads/:fileName',
-  fileHandler.downloadFileController
-);
-router.post('/api/files/upload', fileHandler.upload);
-router.get('/api/files/downloads/', fileHandler.showDownloads);
-router.post('/api/files/uploadNew', fileHandler.uploadNew);
+router.get('/downloads/:fileName', fileHandler.downloadFileController);
+router.post('/upload', fileHandler.upload);
+router.get('/downloads/', fileHandler.showDownloads);
+router.post('/uploadNew', fileHandler.uploadNew);
 module.exports = router;

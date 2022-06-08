e351475df9071a5bca919464ae2965dbb8cd1fba
Kyle Wheeless <kyle@wheeless.dev>
2022-06-08 12:11:35 -0700

    removed dead module from fileHandler controller

--- controllers/fileHandler.js
+++ controllers/fileHandler.js
@@ -207,31 +207,3 @@ exports.uploadNew = async (req, res) => {
     res.status(500).json('Server Error');
   }
 };
-
-exports.slackWebhook = async (req, res) => {
-  let studentEmails = req.body.email.split(' ');
-  console.log(studentEmails);
-  let message = req.body.message;
-  try {
-    const uri = `${process.env.SLACK_WEBHOOK_URL}`;
-
-    const emailLoop = async (studentEmails) => {
-      for (let i = 0; i < studentEmails.length; i++) {
-        await fetch(uri, {
-          method: 'POST',
-          body: JSON.stringify({
-            email: studentEmails[i],
-            message: message,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-      }
-    };
-    emailLoop(studentEmails);
-    console.log('Sent to Slack');
-    return res.status(200).json('Success!');
-  } catch (err) {
-    console.error(err);
-    res.status(500).json('Server Error');
-  }
-};

41a972d11e80308d7af9ab0a2b8394d1a7c34a6b
Kyle Wheeless <kyle@wheeless.dev>
2022-06-08 23:12:27 -0700

    end test of production stability

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const limiter = rateLimit({
   }),
   windowMs: 1 * 60 * 1000, // 1 minute(s)
   message: 'Too many requests from this IP, please try again shortly. Thanks!',
-  max: 10000, // Limit each IP to 60 requests per `window` (here, per 1 minute(s))
+  max: 60, // Limit each IP to 60 requests per `window` (here, per 1 minute(s))
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
